118624da2dae74574ac7c44abac26831bcd100de
Drew Edwards <github@drew.contact>
2023-09-02 15:36:19 +0100

    refactor: remove pro-layout dependency

---
+++ src/layout/PageHeader.tsx
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 Drew Edwards
+// This file is part of KanjiSchool under AGPL-3.0.
+// Full details: https://github.com/Lemmmy/KanjiSchool/blob/master/LICENSE
+
+import { ReactNode } from "react";
+import { Button } from "antd";
+import classNames from "classnames";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+
+interface Props {
+  title?: ReactNode;
+  onBack?: () => void;
+  className?: string;
+}
+
+export function PageHeader({
+  title,
+  onBack,
+  className
+}: Props): JSX.Element {
+  const classes = classNames("min-h-page-header px-lg pt-md pb-0 box-border flex items-center", className);
+
+  return <div className={classes}>
+    {/* Back button */}
+    {onBack && <Button
+      type="link"
+      aria-label="Back"
+      tabIndex={0}
+      onClick={onBack}
+      className="mr-md px-0 text-white flex-shrink-0"
+    >
+      <ArrowLeftOutlined />
+    </Button>}
+
+    {/* Title */}
+    <div className="font-semibold text-[23px] leading-[32px] overflow-hidden whitespace-nowrap text-ellipsis">
+      {title}
+    </div>
+  </div>;
+}
--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -2,18 +2,16 @@
 // This file is part of KanjiSchool under AGPL-3.0.
 // Full details: https://github.com/Lemmmy/KanjiSchool/blob/master/LICENSE
 
-import { FC, HTMLProps, ReactNode, useEffect } from "react";
+import { HTMLProps, ReactNode, useEffect } from "react";
 import classNames from "classnames";
 
 import { useNavigate } from "react-router-dom";
-import { PageHeader } from "@ant-design/pro-layout";
+import { PageHeader } from "@layout/PageHeader.tsx";
 
 export type PageLayoutProps = HTMLProps<HTMLDivElement> & {
   siteTitle?: string;
   title?: ReactNode | string;
-  subTitle?: ReactNode | string;
 
-  extra?: ReactNode;
   noHeader?: boolean;
   centered?: boolean;
   verticallyCentered?: boolean;
@@ -27,13 +25,13 @@ export type PageLayoutProps = HTMLProps<HTMLDivElement> & {
   verticallyCenteredClassName?: string;
   onBack?: () => void;
   backLink?: string;
+
+  children?: ReactNode;
 }
 
-export const PageLayout: FC<PageLayoutProps> = ({
+export function PageLayout({
   siteTitle,
   title,
-  subTitle,
-  extra,
   noHeader,
   centered,
   verticallyCentered,
@@ -56,7 +54,7 @@ export const PageLayout: FC<PageLayoutProps> = ({
   backLink,
   children,
   ...rest
-}) => {
+}: PageLayoutProps): JSX.Element {
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -68,15 +66,8 @@ export const PageLayout: FC<PageLayoutProps> = ({
   return <div className={classes} {...rest}>
     {/* Page header */}
     {!noHeader && title && <PageHeader
-      className={classNames(
-        "min-h-page-header pb-0 box-border [&_.ant-page-header-heading-sub-title_.ant_typography]:text-inherit",
-        headerClassName
-      )}
-
+      className={headerClassName}
       title={title}
-      subTitle={subTitle}
-      extra={extra}
-
       onBack={() => {
         if (onBack) onBack();
         else if (backLink) navigate(backLink);
@@ -94,4 +85,4 @@ export const PageLayout: FC<PageLayoutProps> = ({
       {children}
     </div>
   </div>;
-};
+}
