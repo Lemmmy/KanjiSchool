3403043f20434939ff197d56570eb05f3b1c78bd
Drew Edwards <github@drew.contact>
2023-09-23 02:52:43 +0100

    fix: remove use of Collapse children in search

--- src/pages/search/SearchResultsList.tsx
+++ src/pages/search/SearchResultsList.tsx
@@ -3,7 +3,7 @@
 // Full details: https://github.com/Lemmmy/KanjiSchool/blob/master/LICENSE
 
 import { useState, useEffect, useMemo } from "react";
-import { Tag, Collapse } from "antd";
+import { Tag, Collapse, CollapseProps } from "antd";
 import classNames from "classnames";
 
 import { SearchResultGroup } from "@api";
@@ -21,6 +21,13 @@ interface Props {
   isNested?: boolean;
 }
 
+// Shared props for all the subject lists
+const elProps = {
+  size: "small",
+  alignLeft: false,
+  innerPadding: 16,
+} as const;
+
 export function SearchResultsList({ groups, isNested }: Props): JSX.Element {
   // Active keys for the panel (managed so we can reset them on new results)
   const [activeKeys, setActiveKeys] = useState<string | string[]>();
@@ -32,12 +39,30 @@ export function SearchResultsList({ groups, isNested }: Props): JSX.Element {
   // Reset the active keys if the groups change
   useEffect(() => setActiveKeys(defaultKeys), [defaultKeys]);
 
-  // Shared props for all the subject lists
-  const elProps: { size: "small"; alignLeft: boolean; innerPadding: number } = {
-    size: "small",
-    alignLeft: false,
-    innerPadding: 16,
-  };
+  const items: CollapseProps["items"] = useMemo(() => {
+    return groups.map(group => ({
+      key: group.name,
+
+      label: <CollapseHeader group={group} />,
+      extra: <CollapseHeaderExtra group={group} />,
+
+      children: <>
+        {/* If there are subgroups, render those with another list */}
+        {group.subGroups && <SearchResultsList
+          groups={group.subGroups}
+          isNested
+        />}
+
+        {/* Display the list items */}
+        {group.itemSubjects && group.name === "Radical" &&
+          <SubjectGrid {...elProps} subjectIds={group.itemSubjects} />}
+        {group.itemSubjects && group.name === "Kanji" &&
+          <SubjectGrid {...elProps} subjectIds={group.itemSubjects} />}
+        {group.itemSubjects && group.name === "Vocabulary" &&
+          <VocabList {...elProps} subjectIds={group.itemSubjects} />}
+      </>
+    }));
+  }, [groups]);
 
   const classes = classNames(
     "max-w-[920px] mx-auto my-lg",
@@ -54,50 +79,32 @@ export function SearchResultsList({ groups, isNested }: Props): JSX.Element {
     activeKey={activeKeys}
     defaultActiveKey={defaultKeys}
     onChange={setActiveKeys}
-  >
-    {/* Render each result group as a collapse panel */}
-    {groups.map(group => (
-      <Collapse.Panel
-        key={group.name}
+    items={items}
+  />;
+}
 
-        // Header
-        header={<div className="inline-flex items-center">
-          {/* Group name */}
-          <h2 className="font-medium my-0">{group.name}</h2>
+function CollapseHeader({ group: { count, name } }: { group: SearchResultGroup }) {
+  return <div className="inline-flex items-center">
+    {/* Group name */}
+    <h2 className="font-medium my-0">{name}</h2>
 
-          {/* Total count */}
-          <Tag className="mt-px ml-xs text-sm font-normal">
-            {nts(group.count)}
-          </Tag>
-        </div>}
+    {/* Total count */}
+    <Tag className="mt-px ml-xs text-sm font-normal">
+      {nts(count)}
+    </Tag>
+  </div>;
+}
 
-        // Header extra
-        extra={<div className="flex flex-col items-end justify-center">
-          {/* Counts per SRS stage */}
-          <ResultStageCounts counts={group.srsCounts} />
+function CollapseHeaderExtra({ group: { itemSubjects, srsCounts } }: { group: SearchResultGroup }) {
+  return <div className="flex flex-col items-end justify-center">
+    {/* Counts per SRS stage */}
+    <ResultStageCounts counts={srsCounts} />
 
-          {/* Add to self-study queue button */}
-          {group?.itemSubjects?.length && <StudyQueueButton
-            type="primary"
-            size="small"
-            subjectIds={group.itemSubjects}
-          />}
-        </div>}
-      >
-        {/* If there are subgroups, render those with another list */}
-        {group.subGroups && <SearchResultsList
-          groups={group.subGroups}
-          isNested
-        />}
-
-        {/* Display the list items */}
-        {group.itemSubjects && group.name === "Radical" &&
-          <SubjectGrid {...elProps} subjectIds={group.itemSubjects} />}
-        {group.itemSubjects && group.name === "Kanji" &&
-          <SubjectGrid {...elProps} subjectIds={group.itemSubjects} />}
-        {group.itemSubjects && group.name === "Vocabulary" &&
-          <VocabList {...elProps} subjectIds={group.itemSubjects} />}
-      </Collapse.Panel>
-    ))}
-  </Collapse>;
+    {/* Add to self-study queue button */}
+    {itemSubjects?.length && <StudyQueueButton
+      type="primary"
+      size="small"
+      subjectIds={itemSubjects}
+    />}
+  </div>;
 }
