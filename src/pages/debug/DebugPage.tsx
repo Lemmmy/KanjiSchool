53246897d7c78769536c3fb15cc2f87872868983
Drew Edwards <github@drew.contact>
2023-10-05 19:51:19 +0100

    chore: remove heatmap debug card

--- src/pages/debug/DebugPage.tsx
+++ src/pages/debug/DebugPage.tsx
@@ -2,25 +2,19 @@
 // This file is part of KanjiSchool under AGPL-3.0.
 // Full details: https://github.com/Lemmmy/KanjiSchool/blob/master/LICENSE
 
-import { Space, Card } from "antd";
+import { Space } from "antd";
 
 import { PageLayout } from "@layout/PageLayout";
 
 import { useCustomSessionCard } from "./DebugCustomSessionCard";
 import { DebugSubjectsCard } from "./DebugSubjectsCard";
 import { DebugMiscCard } from "./DebugMiscCard";
-import { Heatmap } from "@pages/dashboard/heatmap/Heatmap";
 
 export function Component(): JSX.Element {
   const [customSessionCard, onAddSubject] = useCustomSessionCard();
 
   return <PageLayout siteTitle="Debug" title="Debug" className="debug-page">
     <Space direction="vertical" size="large">
-      <Card>
-        <div style={{ display: "flex", width: 1080, height: 640 }}>
-          <Heatmap currentYearOnly={false} style={{ flex: 1 }} />
-        </div>
-      </Card>
       {customSessionCard}
       <DebugSubjectsCard onAddSubject={onAddSubject} />
       <DebugMiscCard />
